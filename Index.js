d57834b4c80d23aa6284b03c2914ca4d441bad47
BuildTools <unconfigured@null.spigotmc.org>
2020-01-13 13:20:35 +1000

    Added Files
    Yes

---
+++ Index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Noah Sleeping', {type: 'WATCHING'}).catch(console.error);
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if (!channel) return;
+
+    channel.send(`Welcome To Random Stuff, ${member}, Read Rules in #rules cutie :3!`)
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.channel.send('@everyone');
+  }
+});
+
+client.login('NjY2MTA1ODMyNzQzNzYzOTc4.XhvYCw.IT5sJyRPcsoooAblVJ1AiE_Gajc');
